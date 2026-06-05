32a27514035ec552b923d44503c43fa172591f21
tsanta22Kyle <152197350+tsanta22Kyle@users.noreply.github.com>
2026-06-05 18:23:33 +0300

    Remove FaqSection export from index.ts

--- features/home/components/index.ts
+++ features/home/components/index.ts
@@ -5,7 +5,6 @@ export * from "./ProcessSection";
 export * from "./PortfolioSection";
 export * from "./TestimonialsSection";
 export * from "./CurvedCarousel";
-export * from "./FaqSection";
 export * from "./WhyChooseUsSection";
 export * from "./NewsSection";
 export * from "./CtaSection";
